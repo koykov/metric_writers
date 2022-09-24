8c813293d8a7e702c451b9b03db39933fba365a6
Konstantin Voykov <koykov0@gmail.com>
2022-09-24 23:09:52 +0300

    cbyte issue: avoid using pointer receivers.

--- cbyte/prometheus.go
+++ cbyte/prometheus.go
@@ -17,18 +17,18 @@ func NewPrometheusMetrics() *PrometheusMetrics {
 	return m
 }
 
-func (m *PrometheusMetrics) Alloc(cap uint64) {
+func (m PrometheusMetrics) Alloc(cap uint64) {
 	promAlloc.WithLabelValues().Add(1)
 	promMem.Add(float64(cap))
 }
 
-func (m *PrometheusMetrics) Grow(capOld, cap uint64) {
+func (m PrometheusMetrics) Grow(capOld, cap uint64) {
 	promGrow.WithLabelValues().Add(1)
 	promMem.Sub(float64(capOld))
 	promMem.Add(float64(cap))
 }
 
-func (m *PrometheusMetrics) Free(cap uint64) {
+func (m PrometheusMetrics) Free(cap uint64) {
 	promFree.WithLabelValues().Add(1)
 	promMem.Sub(float64(cap))
 }
