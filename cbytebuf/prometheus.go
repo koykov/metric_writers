613e1ec94cc5b1b05c8101950f782e1e2056a05c
Konstantin Voykov <koykov0@gmail.com>
2022-09-24 23:09:34 +0300

    cbytebuf issue: avoid using pointer receivers.

--- cbytebuf/prometheus.go
+++ cbytebuf/prometheus.go
@@ -17,13 +17,13 @@ func NewPrometheusMetrics() *PrometheusMetrics {
 	return m
 }
 
-func (m *PrometheusMetrics) PoolAcquire(cap uint64) {
+func (m PrometheusMetrics) PoolAcquire(cap uint64) {
 	promCbytebufAcq.WithLabelValues().Add(1)
 	promCbytebufPool.Sub(1)
 	promCbytebufPoolMem.Sub(float64(cap))
 }
 
-func (m *PrometheusMetrics) PoolRelease(cap uint64) {
+func (m PrometheusMetrics) PoolRelease(cap uint64) {
 	promCbytebufRel.WithLabelValues().Add(1)
 	promCbytebufPool.Add(1)
 	promCbytebufPoolMem.Add(float64(cap))
